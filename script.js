3d9e1a8e9b57242dd2b9a66ebde77e334652a1cb
Joseph Petersen <joseph.j.petersen@gmail.com>
2022-07-26 14:11:45 -0400

    Add clear button

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const container = document.getElementById("container");
 const sizeValue = document.getElementById("size-value");
 const sizeSlider = document.getElementById("size-slider");
 const colorChoices = Array.from(document.querySelectorAll(".color-choice"));
+const clearButton = document.getElementById("clear");
 
 let colorChoice = 'black';
 let gridItems = Array.from(document.querySelectorAll('.grid-item'));
 
 function makeGrid(rows, columns, color) {
+  
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-colums', columns);
+  
   for (i = 0; i < (rows * columns); i++) {
     let cell = document.createElement('div');
     container.appendChild(cell).className = 'grid-item';
@@ -61,6 +64,12 @@ colorChoices.forEach((color) =>
     colorChoice = e.target.value;
     checkColor();
   })
+
+clearButton.onclick = function() {
+  for (var gridItem of gridItems) {
+    gridItem.style.backgroundColor = '';
+  }
+}
 
 sizeSlider.oninput = function() {
   sizeValue.innerText = `${this.value} x ${this.value}`;
