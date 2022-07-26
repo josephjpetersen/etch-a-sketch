df48d8bea6a910967e4672bb91801dccad065bb6
Joseph Petersen <joseph.j.petersen@gmail.com>
2022-07-26 13:52:43 -0400

    Script radio button color change

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const container = document.getElementById("container");
 const sizeValue = document.getElementById("size-value");
 const sizeSlider = document.getElementById("size-slider");
+const colorChoices = Array.from(document.querySelectorAll(".color-choice"));
 
+let colorChoice = 'black';
 let gridItems = Array.from(document.querySelectorAll('.grid-item'));
 
 function makeGrid(rows, columns, color) {
@@ -14,16 +16,56 @@ function makeGrid(rows, columns, color) {
 
   gridItems = Array.from(document.querySelectorAll('.grid-item'));
 
-  gridItems.forEach((gridItem) =>
-    gridItem.addEventListener('mouseover',
-      e => e.target.style.backgroundColor = color)
-  )
+  checkColor();
+
 };
+
+function removeAllChildren(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function randomRGB() {
+  let red = Math.floor((Math.random() * 255) + 1);
+  let green = Math.floor((Math.random() * 255) + 1);
+  let blue = Math.floor((Math.random() * 255) + 1);
+
+  return `rgb(${red},${green},${blue})`;
+}
+
+function checkColor() {
+  if (colorChoice === 'black') {
+    gridItems.forEach((gridItem) =>
+      gridItem.addEventListener('mouseover',
+        e => {
+          e.target.style.backgroundColor = 'black';
+        }
+      )
+    )
+  } else if (colorChoice === 'chaos') {
+    gridItems.forEach((gridItem) =>
+      gridItem.addEventListener('mouseover',
+        e => {
+          e.target.style.backgroundColor = randomRGB();
+        }
+      )
+    )
+  }
+}
 
 makeGrid(16, 16, 'black');
 
+colorChoices.forEach((color) =>
+  color.onclick = (e) => {
+    colorChoice = e.target.value;
+    checkColor();
+  })
+
 sizeSlider.oninput = function() {
   sizeValue.innerText = `${this.value} x ${this.value}`;
+  removeAllChildren(container);
+  makeGrid(this.value, this.value, colorChoice);
 };
 
 
