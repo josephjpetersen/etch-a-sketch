8f9e50733a5dea6099aaebf6e1dc3798a0beba7a
Joseph Petersen <joseph.j.petersen@gmail.com>
2022-07-26 11:32:50 -0400

    Script basic mouseover sketch functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.getElementById("container");
+let gridItems = Array.from(document.querySelectorAll('.grid-item'));
 
 function makeGrid(rows, columns) {
   container.style.setProperty('--grid-rows', rows);
@@ -7,6 +8,18 @@ function makeGrid(rows, columns) {
     let cell = document.createElement('div');
     container.appendChild(cell).className = 'grid-item';
   };
+
+  gridItems = Array.from(document.querySelectorAll('.grid-item'));
+
+  gridItems.forEach((gridItem) =>
+    gridItem.addEventListener('mouseover',
+      e => e.target.classList.add('black-background')
+    )
+  )
 };
 
-makeGrid(16, 16)
+makeGrid(16, 16);
+
+
+
+
